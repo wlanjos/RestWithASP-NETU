f38a09fb0c2730d3192e1ef227c3e584a53fbd0e
Washington Luiz dos Anjos <wlanjos@hotmail.com>
2019-09-26 15:25:53 -0300

    Implemementing all principal verbs

---
+++ RestWithASPNETU 02 - Using Diferent Verbs/RestWithASPNETU/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETU.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Andress { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
