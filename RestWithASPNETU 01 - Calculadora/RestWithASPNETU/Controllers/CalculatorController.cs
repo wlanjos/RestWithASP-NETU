14b9241875d332721ea6637b885eba03f2605ce1
Washington Luiz dos Anjos <wlanjos@hotmail.com>
2019-09-26 12:27:43 -0300

    Implementing REST Calculator

--- RestWithASPNETU 01 - Calculadora/RestWithASPNETU/Controllers/CalculatorController.cs
+++ RestWithASPNETU 01 - Calculadora/RestWithASPNETU/Controllers/CalculatorController.cs
@@ -8,7 +8,7 @@ namespace RestWithASPNETU.Controllers
     {
 
 
-        // GET api/values/5/5
+        // GET api/values/sum/5/5
         [HttpGet("{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
@@ -19,6 +19,47 @@ namespace RestWithASPNETU.Controllers
             }
             return BadRequest("Invalid Input");
         }
+
+        // GET api/values/subtaction/5/5
+        //******************
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/multiplication/5/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+
+        public IActionResult multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/division/5/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+
+        public IActionResult division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
 
         private decimal ConvertToDecimal(string number)
         {
